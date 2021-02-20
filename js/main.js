c7f562ecc9be441b6da07311c041dfffc2a53a0a
Aleksander Voronov <irreligious86@gmail.com>
2021-02-20 21:15:25 +0100

    finish adaptive layout

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const prev = document.getElementById('control-left');
+const next = document.getElementById('control-right');
+const slides = document.getElementsByClassName('slide');
+console.log(slides)
+console.log(prev);
+console.log(next);
