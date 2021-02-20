dcf771b6c7256bcd4666de409e990e9202ea8a2a
Aleksander Voronov <irreligious86@gmail.com>
2021-02-21 00:49:51 +0100

    init: add slider

--- js/main.js
+++ js/main.js
@@ -1,6 +1,52 @@
 const prev = document.getElementById('control-left');
 const next = document.getElementById('control-right');
-const slides = document.getElementsByClassName('slide');
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot-bar-item');
+let index = 0;
+console.log(dots);
 console.log(slides)
 console.log(prev);
 console.log(next);
+
+const activeSlide = n => {
+    for (slide of slides) {
+        slide.classList.add('hidden');
+    }
+    slides[n].classList.remove('hidden');
+};
+
+const activeDot = n => {
+    for (dot of dots) {
+        dot.classList.remove('dot-active');
+    }
+    dots[n].classList.add('dot-active');
+};
+
+const prepareCurrentSlide = ind => {
+    activeSlide(ind);
+    activeDot(ind);
+}
+
+const prevSlide = () => {
+    if (index == 0) {
+        index = slides.length - 1;
+        prepareCurrentSlide(index);
+    } else {
+        index--;
+        prepareCurrentSlide(index);
+    }
+};
+
+const nextSlide = () => {
+    if (index == slides.length - 1) {
+        index = 0;
+        prepareCurrentSlide(index);
+    } else {
+        index++;
+        prepareCurrentSlide(index);
+    }
+};
+
+
+prev.addEventListener('click', prevSlide);
+next.addEventListener('click', nextSlide);
